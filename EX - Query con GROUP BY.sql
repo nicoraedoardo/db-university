0480eb6046d29b88514175a14c2b363bbd7c3b56
nicoraedoardo <edoardo.nicora26@gmail.com>
2026-03-12 16:51:29 +0100

    second query

---
+++ EX - Query con GROUP BY.sql
@@ -0,0 +1,35 @@
+1. Contare quanti iscritti ci sono stati ogni anno
+
+SELECT 
+    YEAR(enrolment_date) AS anno, 
+    COUNT(*) AS iscritti_totali
+FROM students
+GROUP BY YEAR(enrolment_date)
+ORDER BY anno;
+
+2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+
+SELECT 
+    office_address, 
+    COUNT(*) AS numero_insegnanti
+FROM teachers
+GROUP BY office_address
+HAVING COUNT(*) > 1;
+
+3. Calcolare la media dei voti di ogni appello d'esame
+
+SELECT 
+    exam_id, 
+    AVG(vote) AS media_voti
+FROM exam_student
+GROUP BY exam_id;
+
+
+
+4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+
+SELECT 
+    department_id, 
+    COUNT(*) AS numero_corsi_laurea
+FROM degrees
+GROUP BY department_id;
